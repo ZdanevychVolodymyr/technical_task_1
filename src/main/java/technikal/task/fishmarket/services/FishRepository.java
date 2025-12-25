dc19d8aa6027b71c7a9c9c67eee18419ef28a1a4
zdanevychvolodymyr1 <zdanevychvolodymyr1@gmail.com>
2025-12-25 21:14:28 +0200

    Initial commit for the new repository

--- src/main/java/technikal/task/fishmarket/services/FishRepository.java
+++
@@ -1,9 +0,0 @@
-package technikal.task.fishmarket.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import technikal.task.fishmarket.models.Fish;
-
-public interface FishRepository extends JpaRepository<Fish, Integer> {
-
-}
